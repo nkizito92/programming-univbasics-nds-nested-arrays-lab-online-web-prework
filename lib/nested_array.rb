738814fb1a5113486cfe6dc4d99a730ad620a453
Kizito Njoku <nkizito57@gmail.com>
2019-10-22 20:18:09 +0000

    Done.

--- lib/nested_array.rb
+++ lib/nested_array.rb
@@ -32,12 +32,12 @@ end
 
 def matrix_lookup(matrix, row, column)
   # Return the matrix's content at row and column
-  matrix[1][1]
+  matrix[row][column]
 end
 
 def matrix_update(matrix, row, column, new_value)
   # Update the matrix location at row and column to have the value of new_value
   # Return the updated matrix
-  matrix[0][2] = new_value
- puts matrix[0][2]
+matrix[row][column] = new_value
+matrix
 end
